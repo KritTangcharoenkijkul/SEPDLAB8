816f07501b360ee2f37d5fa166c4db0fd66f1376
KritTangcharoenkijkul <48550332+KritTangcharoenkijkul@users.noreply.github.com>
2019-03-28 14:04:33 +0700

    added zelda triangle

--- Q1_Krit.py
+++ Q1_Krit.py
@@ -2,6 +2,7 @@ import sys
 from PySide2.QtCore import *
 from PySide2.QtWidgets import *
 from PySide2.QtGui import *
+from Q1Code import Simple_drawing_window
 
 class Simple_drawing_window(QWidget):
     def __init__(self):
@@ -15,20 +16,20 @@ class Simple_drawing_window(QWidget):
 
         p.setPen(QColor(0, 0, 0))
         p.setBrush(QColor(0, 127, 0))
-        p.drawPolygon([
-            QPoint(70, 100), QPoint(100,110),
-            QPoint(130,100), QPoint(100,150),
-        ])
 
-        p.setPen(QColor(255, 127, 0))
-        p.setBrush(QColor(255,127,0))
-        p.drawPie(50, 150, 100, 100,0 ,180 * 15)
-
+        p.setPen(QColor(255, 255, 0))
+        p.setBrush(QColor(255, 255, 0))
         p.drawPolygon(
-            [QPoint(50, 200), QPoint(150, 200), QPoint(100, 400)]
+            [QPoint(50, 200), QPoint(250, 200), QPoint(150, 400)]
         )
 
-        p.drawPixmap(QRect(200, 100, 320,320), self.rabbit)
+        p.setPen(QColor(255, 255, 255))
+        p.setBrush(QColor(255, 255, 255))
+        p.drawPolygon(
+            [QPoint(100, 300), QPoint(200, 300), QPoint(150, 200)]
+        )
+
+        p.drawPixmap(QRect(200, 100, 320, 320), self.rabbit)
         p.end()
 
 def main():
